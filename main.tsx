5561778a8014d83bab6beb498857e9c7cf5f522a
hotsome <hotsome.sakino@gmail.com>
2025-07-04 12:57:29 +0900

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const App = () => <h1>Hello, Photo Filter!</h1>;
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
